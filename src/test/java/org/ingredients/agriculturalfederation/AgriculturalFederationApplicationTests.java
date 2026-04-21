4298cdf5ec1f5a4c7823f6afc6a8e9709e95d258
mayahNeko <mayahibela@gmail.com>
2026-04-21 13:33:00 +0300

    Initial commit

---
+++ src/test/java/org/ingredients/agriculturalfederation/AgriculturalFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ingredients.agriculturalfederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
