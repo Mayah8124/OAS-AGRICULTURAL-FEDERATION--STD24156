0b70e76b49724fe908cb0e6cd7222044f437bfba
mayahNeko <mayahibela@gmail.com>
2026-04-22 23:05:53 +0300

    feat(repository): add transaction repository

---
+++ src/main/java/org/ingredients/agriculturalfederation/repository/CollectivityTransactionRepository.java
@@ -0,0 +1,14 @@
+package org.ingredients.agriculturalfederation.repository;
+
+import org.ingredients.agriculturalfederation.entity.CollectivityTransaction;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CollectivityTransactionRepository {
+    List<CollectivityTransaction> findByCollectivityIdAndDateRange(String collectivityId, LocalDate fromDate, LocalDate toDate);
+    boolean existsById(String collectivityId);
+    CollectivityTransaction save(CollectivityTransaction transaction);
+    CollectivityTransaction findById(String transactionId);
+    List<CollectivityTransaction> findByCollectivityId(String collectivityId);
+}
