989c4f70457a8724bdd2bb3a23edcfe7a59f2df2
mayahNeko <mayahibela@gmail.com>
2026-04-22 23:12:58 +0300

    feat(service): add transaction service

--- src/main/java/org/ingredients/agriculturalfederation/repository/CollectivityTransactionRepository.java
+++ src/main/java/org/ingredients/agriculturalfederation/repository/CollectivityTransactionRepository.java
@@ -1,10 +1,12 @@
 package org.ingredients.agriculturalfederation.repository;
 
 import org.ingredients.agriculturalfederation.entity.CollectivityTransaction;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface CollectivityTransactionRepository {
     List<CollectivityTransaction> findByCollectivityIdAndDateRange(String collectivityId, LocalDate fromDate, LocalDate toDate);
     boolean existsById(String collectivityId);
--- src/main/java/org/ingredients/agriculturalfederation/service/TransactionsService.java
+++ src/main/java/org/ingredients/agriculturalfederation/service/TransactionsService.java
@@ -1,7 +1,31 @@
 package org.ingredients.agriculturalfederation.service;
 
+import org.ingredients.agriculturalfederation.entity.CollectivityTransaction;
+import org.ingredients.agriculturalfederation.repository.CollectivityTransactionRepository;
+import org.ingredients.agriculturalfederation.validator.TransactionsValidator;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class TransactionsService {
+    private final CollectivityTransactionRepository collectivityTransactionRepository;
+    private final TransactionsValidator transactionsValidator;
+
+    public TransactionsService(CollectivityTransactionRepository collectivityTransactionRepository, TransactionsValidator transactionsValidator) {
+        this.collectivityTransactionRepository = collectivityTransactionRepository;
+        this.transactionsValidator = transactionsValidator;
+    }
+
+    public List<CollectivityTransaction> getCollectivityTransactionsBetween(
+            String collectivityId,
+            LocalDate from,
+            LocalDate to
+    ) {
+        transactionsValidator.validateParameters(collectivityId, from, to);
+        boolean collectivityExists = collectivityTransactionRepository.existsById(collectivityId);
+        transactionsValidator.validateCollectivityExists(collectivityId, collectivityExists);
+        return collectivityTransactionRepository.findByCollectivityIdAndDateRange(collectivityId, from, to);
+    }
 }
