3cc0cacacc70b0e1a2c75dfc34753174144982e6
mayahNeko <mayahibela@gmail.com>
2026-04-22 23:23:52 +0300

    feat(controller): add transaction controller

---
+++ src/main/java/org/ingredients/agriculturalfederation/controller/TransactionsController.java
@@ -0,0 +1,28 @@
+package org.ingredients.agriculturalfederation.controller;
+
+import org.ingredients.agriculturalfederation.entity.CollectivityTransaction;
+import org.ingredients.agriculturalfederation.service.TransactionsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class TransactionsController {
+    private final TransactionsService transactionsService;
+
+    public TransactionsController(TransactionsService transactionsService) {
+        this.transactionsService = transactionsService;
+    }
+
+    @GetMapping("/collectivities/{id}/transactions")
+    public ResponseEntity<List<CollectivityTransaction>> getTransactionsBetween(
+            @PathVariable String id,
+            @RequestParam String from,
+            @RequestParam String to
+    ) {
+        List<CollectivityTransaction> transactions = transactionsService.getCollectivityTransactionsBetween(id, LocalDate.parse(from), LocalDate.parse(to));
+        return ResponseEntity.ok(transactions);
+    }
+}
