2fee7937b4013d11365a9c6227d106f1b631c9b7
mayahNeko <mayahibela@gmail.com>
2026-04-22 19:04:36 +0300

    feat(service): implement membership fees service

--- src/main/java/org/ingredients/agriculturalfederation/service/MembershipFeeService.java
+++ src/main/java/org/ingredients/agriculturalfederation/service/MembershipFeeService.java
@@ -1,8 +1,24 @@
 package org.ingredients.agriculturalfederation.service;
 
+import org.ingredients.agriculturalfederation.entity.MembershipFee;
+import org.ingredients.agriculturalfederation.repository.MembershipFeeRepository;
+import org.ingredients.agriculturalfederation.validator.MembershipFeeValidator;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MembershipFeeService {
+    private final MembershipFeeValidator membershipFeeValidator;
+    private final MembershipFeeRepository membershipFeeRepository;
 
+    public MembershipFeeService(MembershipFeeValidator membershipFeeValidator, MembershipFeeRepository membershipFeeRepository) {
+        this.membershipFeeValidator = membershipFeeValidator;
+        this.membershipFeeRepository = membershipFeeRepository;
+    }
+
+    public List<MembershipFee> getMembershipFees(String collectivityId) {
+        membershipFeeValidator.validateCollectivityId(collectivityId);
+        return membershipFeeRepository.findByCollectivityId(collectivityId);
+    }
 }
