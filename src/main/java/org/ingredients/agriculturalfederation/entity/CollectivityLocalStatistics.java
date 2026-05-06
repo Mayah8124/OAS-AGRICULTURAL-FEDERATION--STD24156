8b11c4b722cead04378cef4a0b39ffeb99794296
mayahNeko <mayahibela@gmail.com>
2026-05-06 10:03:47 +0300

    feat(statistic): add initial and missing classes for statistics

---
+++ src/main/java/org/ingredients/agriculturalfederation/entity/CollectivityLocalStatistics.java
@@ -0,0 +1,14 @@
+package org.ingredients.agriculturalfederation.entity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class CollectivityLocalStatistics {
+    private MemberDescription memberDescription;
+    private double earnedAmount;
+    private double unpaidAmount;
+}
---
+++ src/main/java/org/ingredients/agriculturalfederation/entity/CollectivityOverallSatistics.java
@@ -0,0 +1,14 @@
+package org.ingredients.agriculturalfederation.entity;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class CollectivityOverallStatistics {
+    private CollectivityInformation collectivityInformation;
+    private Integer newMembersNumber;
+    private Double overallMemberCurrentDuePercentage;
+}
